68cc6b9ad37f0965114ab6c413146e301d799f2c
Shoxruzbek7707 <qoshoqboyevshoxruz778@gmail.com>
2025-03-16 14:29:05 +0500

    formatted code

--- src/School.java
+++ src/School.java
@@ -4,50 +4,55 @@ public class School {
     private int teacherCount = 0;
     private int studentCount = 0;
 
-    public School(){
+    public School() {
         this.teachers = new Teacher[100];
         this.students = new Student[100];
     }
 
-    public Teacher[] getTeachers(){
+    public Teacher[] getTeachers() {
         return teachers;
     }
 
-    public Student[] getStudents(){
+    public Student[] getStudents() {
         return students;
     }
 
-    public double getTotalMoneyEarned(){
+    public double getTotalMoneyEarned() {
         double total = 0;
-        for (int i = 0;i < studentCount;i++){
+
+        for (int i = 0; i < studentCount; i++) {
             total += students[i].getFeesPaid();
         }
+
         return total;
     }
 
-    public double getTotalMoneySpent(){
+    public double getTotalMoneySpent() {
         double total = 0;
-        for (int i = 0;i < teacherCount;i++){
+        for (int i = 0; i < teacherCount; i++) {
             total += teachers[i].getSalaryEarned();
         }
+
         return total;
     }
 
 
-    public void addTeacher(Teacher teacher){
-        if (teacherCount >= teachers.length){
-            Teacher[] temp  = new Teacher[teachers.length * 2];
-            System.arraycopy(teachers,0,temp,0,teachers.length);
+    public void addTeacher(Teacher teacher) {
+
+        if (teacherCount >= teachers.length) {
+            Teacher[] temp = new Teacher[teachers.length * 2];
+            System.arraycopy(teachers, 0, temp, 0, teachers.length);
             teachers = temp;
         }
+
         teachers[teacherCount++] = teacher;
         teacher.setSchool(this);
     }
 
-    public void addStudent(Student student){
-        if (studentCount >= students.length){
-            Student[] temp = new  Student[students.length * 2];
-            System.arraycopy(students,0,temp,0,students.length);
+    public void addStudent(Student student) {
+        if (studentCount >= students.length) {
+            Student[] temp = new Student[students.length * 2];
+            System.arraycopy(students, 0, temp, 0, students.length);
             students = temp;
         }
         students[studentCount++] = student;
